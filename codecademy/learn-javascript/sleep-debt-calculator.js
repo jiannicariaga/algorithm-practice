8e93dfb2f8384f99097a738ee2c62203bd1d6731
Jianni Cariaga <jiannicariaga@gmail.com>
2022-12-14 06:58:04 +0000

    Completed the Sleep Debt Calculator project.

---
+++ codecademy/learn-javascript/sleep-debt-calculator.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-console */
+
+const getActualSleepHours = () => 6 + 5 + 5 + 7 + 10 + 8 + 7;
+const getIdealSleepHours = hours => hours * 7;
+const calculateSleepDebt = () => {
+  const actualSleepHours = getActualSleepHours();
+  const idealSleepHours = getIdealSleepHours(8);
+  if (actualSleepHours === idealSleepHours) console.log('you got the perfect amount of sleep.');
+  else if (actualSleepHours > idealSleepHours) console.log('you got more sleep than needed.');
+  else if (actualSleepHours < idealSleepHours) console.log('you should get some rest.');
+};
+
+calculateSleepDebt();

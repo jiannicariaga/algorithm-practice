5042c94878a240bf2a4db4ba248e6b6eac1b03c7
Jianni Cariaga <jiannicariaga@gmail.com>
2023-07-20 17:16:34 +0000

    Added solution for Ada Number challenge.

---
+++ codesignal/arcade/the-core/ada-number.js
@@ -0,0 +1,12 @@
+function solution(line) {
+  line = line.replace(/_/g, '');
+  if (line.match(/^\d+$/)) return true;
+  const num = line.match(/^(\d{1,2})#([0-9a-f]+)#$/i);
+  return num
+    ? [...num[2]].every(v => !isNaN(parseInt(v, num[1]))) && num[1] >= 2 && num[1] <= 16
+    : false;
+}
+
+solution('123_456_789');
+
+// (╯°□°)╯︵ ┻━┻ (╯°□°)╯︵ ┻━┻ (╯°□°)╯︵ ┻━┻
